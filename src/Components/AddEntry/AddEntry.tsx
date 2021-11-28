728a05e99377b75f56e4de66c7ba2532b953e401
saikrishna28 <saikrishnasmarty@gmail.com>
2021-11-28 21:47:16 +0530

    Initial commit - setting up prj

---
+++ src/Components/AddEntry/AddEntry.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface AddEntryProps {}
+
+const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
+  return <h1>Entry Page</h1>;
+};
+
+export default AddEntry;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,57 @@
+import { AppBar, Badge, IconButton, Typography } from "@mui/material";
+import React, { Component } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { Toolbar } from "@mui/material";
+import "./Header.scss";
+import { Box } from "@mui/material";
+
+interface HeaderProps {}
+
+const Header: React.FunctionComponent<HeaderProps> = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Annapurna Car Travels
+          </Typography>
+          <IconButton
+            size="large"
+            aria-label="show 17 new notifications"
+            color="inherit"
+          >
+            <Badge badgeContent={3} color="error">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton
+            size="large"
+            edge="end"
+            aria-label="account of current user"
+            aria-controls="primary-search-account-menu"
+            aria-haspopup="true"
+            onClick={() => {
+              console.log("account log");
+            }}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header;
