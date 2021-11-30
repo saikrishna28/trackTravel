07bb89ec335c05fd23beff5e636155a2dd7ed954
saikrishna28 <saikrishnasmarty@gmail.com>
2021-11-30 14:36:31 +0530

    making date field work

--- src/Components/AddEntry/AddEntry.tsx
+++ src/Components/AddEntry/AddEntry.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   FormControl,
+  Input,
   InputLabel,
   MenuItem,
   Paper,
@@ -8,12 +9,42 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { DatePicker, LocalizationProvider } from "@mui/lab";
 import "./AddEntry.scss";
 
 interface AddEntryProps {}
 
 const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
+  const [custName, setCustName] = useState("");
+  const [custNum, setCustNum] = useState(0);
+  const [custId, setCustId] = useState("");
+
+  const [journeyFrom, setJourneyFrom] = useState("");
+  const [journeyTo, setJourneyTo] = useState("");
+  const [journeyDate, setJourneyDate] = useState<Date | null>(new Date());
+
+  const [vehicleNum, setVehicleNum] = useState("--Select--");
+  const [driverName, setDriverName] = useState("");
+  const [driverNum, setDriverNum] = useState(0);
+  const [openingReading, setOpeningReading] = useState(0);
+  const [endReading, setEndReading] = useState(0);
+  const [totalReading, setTotalReading] = useState(0);
+
+  const [paymentType, setPaymentType] = useState("Cash");
+  const [tollFee, setTollFee] = useState(0);
+  const [kmDriven, setKmDriven] = useState(0);
+  const [driverBeta, setDriverBeta] = useState(0);
+  const [advance, setAdvance] = useState(0);
+  const [parkingFee, setParkingFee] = useState(0);
+  const [costPerKm, setCostPerKm] = useState(0);
+  const [totalPay, setTotalPay] = useState(0);
+  const [netPay, setNetPay] = useState(0);
+
+  console.log(journeyDate);
+
   return (
     <div className="add-entry">
       <div className="add-entry__container">
@@ -38,16 +69,29 @@ const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
                 className="entry-input"
                 label="Name"
                 variant="outlined"
+                value={custName}
+                onChange={(newValue) => {
+                  setCustName(newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="Mobile"
                 variant="outlined"
+                type="number"
+                value={custNum}
+                onChange={(newValue) => {
+                  setCustNum(+newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="ID Proof"
                 variant="outlined"
+                value={custId}
+                onChange={(newValue) => {
+                  setCustId(newValue.target.value);
+                }}
               />
             </div>
           </div>
@@ -70,18 +114,30 @@ const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
                 className="entry-input"
                 label="From"
                 variant="outlined"
+                value={journeyFrom}
+                onChange={(newValue) => {
+                  setJourneyFrom(newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="To"
                 variant="outlined"
+                value={journeyTo}
+                onChange={(newValue) => {
+                  setJourneyTo(newValue.target.value);
+                }}
               />
-              <TextField
-                className="entry-input"
-                label="DOJ"
-                variant="outlined"
-                type="date"
-              />
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="DOJ"
+                  value={journeyDate}
+                  onChange={(newValue) => {
+                    setJourneyDate(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
             </div>
           </div>
         </Paper>
@@ -104,12 +160,14 @@ const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
                 <Select
                   className="entry-input"
                   labelId="vehicle-select-label"
-                  value={"AP28DS9999"}
+                  value={vehicleNum}
                   label="Vehicle num"
-                  //   onChange={handleChange}
+                  onChange={(newValue) => {
+                    setVehicleNum(newValue.target.value);
+                  }}
                 >
+                  <MenuItem value={"--Select--"}>--Select--</MenuItem>
                   <MenuItem value={"AP28DS9999"}>AP28DS9999</MenuItem>
-                  <MenuItem value={"UPI"}>UPI</MenuItem>
                   <MenuItem value={"Card"}>Card</MenuItem>
                 </Select>
               </FormControl>
@@ -117,36 +175,56 @@ const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
                 className="entry-input"
                 label="Driver Name"
                 variant="outlined"
+                value={driverName}
+                onChange={(newValue) => {
+                  setDriverName(newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="Driver Number"
                 variant="outlined"
                 type="number"
+                value={driverNum}
+                onChange={(newValue) => {
+                  setDriverNum(+newValue.target.value);
+                }}
               />
-              <TextField
+              {/* <TextField
                 className="entry-input"
                 label="Total Reading"
                 variant="outlined"
                 type="number"
-              />
+              /> */}
               <TextField
                 className="entry-input"
                 label="Opening Reading"
                 variant="outlined"
                 type="number"
+                value={openingReading}
+                onChange={(newValue) => {
+                  setOpeningReading(+newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="End Reading"
                 variant="outlined"
                 type="number"
+                value={endReading}
+                onChange={(newValue) => {
+                  setEndReading(+newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="Total Reading"
                 variant="outlined"
                 type="number"
+                value={totalReading}
+                onChange={(newValue) => {
+                  setTotalReading(+newValue.target.value);
+                }}
               />
             </div>
           </div>
@@ -171,9 +249,11 @@ const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
                   className="entry-input"
                   labelId="payment-type-select-label"
                   // id="demo-simple-select"
-                  value={"Cash"}
+                  value={paymentType}
                   label="Type"
-                  //   onChange={handleChange}
+                  onChange={(newValue) => {
+                    setPaymentType(newValue.target.value);
+                  }}
                 >
                   <MenuItem value={"Cash"}>Cash</MenuItem>
                   <MenuItem value={"UPI"}>UPI</MenuItem>
@@ -185,6 +265,10 @@ const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
                 label="Advance Paid"
                 variant="outlined"
                 type="number"
+                value={advance}
+                onChange={(newValue) => {
+                  setAdvance(+newValue.target.value);
+                }}
               />
             </div>
             <div>
@@ -193,18 +277,30 @@ const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
                 label="Toll Charges"
                 variant="outlined"
                 type="number"
+                value={tollFee}
+                onChange={(newValue) => {
+                  setTollFee(+newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="Parking Charges"
                 variant="outlined"
                 type="number"
+                value={parkingFee}
+                onChange={(newValue) => {
+                  setParkingFee(+newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="Driver Beta"
                 variant="outlined"
                 type="number"
+                value={driverBeta}
+                onChange={(newValue) => {
+                  setDriverBeta(+newValue.target.value);
+                }}
               />
             </div>
             <div>
@@ -213,12 +309,20 @@ const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
                 label="KMs driven"
                 variant="outlined"
                 type="number"
+                value={kmDriven}
+                onChange={(newValue) => {
+                  setKmDriven(+newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="Cost per KM"
                 variant="outlined"
                 type="number"
+                value={costPerKm}
+                onChange={(newValue) => {
+                  setCostPerKm(+newValue.target.value);
+                }}
               />
             </div>
             <div>
@@ -227,12 +331,20 @@ const AddEntry: React.FunctionComponent<AddEntryProps> = () => {
                 label="Net Payable"
                 variant="outlined"
                 type="number"
+                value={netPay}
+                onChange={(newValue) => {
+                  setNetPay(+newValue.target.value);
+                }}
               />
               <TextField
                 className="entry-input"
                 label="Total"
                 variant="outlined"
                 type="number"
+                value={totalPay}
+                onChange={(newValue) => {
+                  setTotalPay(+newValue.target.value);
+                }}
               />
             </div>
           </div>
